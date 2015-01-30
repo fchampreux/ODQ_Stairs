3f3b981906cf7e770dd4df2d1018445870de1b30
Frédéric Champreux <fchampreux@gmail.com>
2015-01-30 13:37:18 +0100

    PG functions WIP

--- db/datawarehouse/PGSQL/ODQ_Functions.sql
+++ db/datawarehouse/PGSQL/ODQ_Functions.sql
@@ -1,33 +1,44 @@
-create or replace function odq_dwh.bitAND2 (parm1 IN integer, parm2 IN integer) RETURN integer
-IS
+create or replace function odq_app.bitAND2 (parm1 integer, parm2 integer) RETURNS integer
+AS $$
 output_value integer;
 BEGIN
-output_value :=  bitAnd(parm1,parm2);
-return output_value;
-END bitAND2;
-/
+output_value :=  parm1 & parm2;
+RETURNS output_value;
+END ;
+$$ LANGUAGE plpgsql;
 
-create or replace function odq_dwh.bitAND3 (parm1 IN integer, parm2 IN integer, parm3 IN integer) RETURN integer
+********* Example
+
+create or replace function bitAND2 (parm1 integer, parm2 integer) RETURNS integer
+AS $$
+select parm1 & parm2;
+$$ LANGUAGE SQL;
+
+select bitAND2(13,15)
+
+******************
+
+create or replace function odq_app.bitAND3 (parm1 IN integer, parm2 IN integer, parm3 IN integer) RETURNS integer
 IS
 output_value integer;
 BEGIN
 output_value :=  bitAnd(bitAnd(parm1,parm2),parm3);
-return output_value;
+RETURNS output_value;
 END bitAND3;
 /
 
-create or replace function odq_dwh.bitOR (parm1 IN integer, parm2 IN integer) RETURN integer
+create or replace function odq_app.bitOR (parm1 IN integer, parm2 IN integer) RETURNS integer
 IS
 output_value integer;
 BEGIN
 output_value := (parm1+parm2) - bitAnd(parm1,parm2);
-return output_value;
+RETURNS output_value;
 END bitOR;
 /
 
-CREATE OR REPLACE FUNCTION odq_dwh.to_binary
+CREATE OR REPLACE FUNCTION odq_app.to_binary
 (pin IN NUMBER)
-RETURN VARCHAR2 
+RETURNS VARCHAR2 
 IS
 v_in   NUMBER;
 v_next NUMBER;
@@ -38,13 +49,13 @@ v_next := mod(v_in,2);
 v_result := to_char(v_next) || v_result;
 v_in := floor(v_in / 2);
 END LOOP;
-RETURN v_result;
+RETURNS v_result;
 END;
 /
 
-CREATE OR REPLACE FUNCTION odq_dwh.from_binary 
+CREATE OR REPLACE FUNCTION odq_app.from_binary 
 (pin IN VARCHAR2) 
-RETURN NUMBER
+RETURNS NUMBER
 IS
 c_in VARCHAR(2000);
 c_dim NUMBER;
@@ -65,6 +76,6 @@ v_power := c_dim - v_cursor;
 v_result := v_result + (v_bit * power(2,v_power)) ;
 v_cursor := v_cursor - 1;
 END LOOP;
-RETURN v_result;
+RETURNS v_result;
 END;
 /
