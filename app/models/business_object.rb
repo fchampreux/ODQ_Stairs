a73bd1a8ff9575e7424167f7639ce79e2c236cdc
Frédéric Champreux <fchampreux@gmail.com>
2016-04-11 21:23:40 +0200

    cleanup

--- app/models/business_object.rb
+++ app/models/business_object.rb
@@ -53,7 +53,7 @@ self.sequence_name = "global_seq"
   belongs_to :status, :class_name => "Parameter", :foreign_key => "status_id"	# helps retrieving the status name
   belongs_to :business_area
   has_many :business_rules
-  has_many :columns
+  has_many :columns, :dependent => :destroy
   belongs_to :main_scope, :class_name => "Scope", :foreign_key => "main_scope_id"
   accepts_nested_attributes_for :columns
 
