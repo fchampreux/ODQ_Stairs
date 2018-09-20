050d73a9021b8f1c04382aff87c869ae5de56c12
Frédéric CHAMPREUX <fchampreux@gmail.com>
2018-09-20 17:25:33 +0200

    Update FakeData_PG.sql

--- db/Datawarehouse/PGSQL/FakeData_PG.sql
+++ db/Datawarehouse/PGSQL/FakeData_PG.sql
@@ -97,26 +97,6 @@ added_value = R.added_value,
 maintenance_cost = R.maintenance_cost
 from ( select odq_parent_id,
 sum(workload) workload,
-sum(added_value) added_value,
-sum(maintenance_cost) maintenance_cost from odq_dwh.dm_processes 
-where odq_object_id < 100
-group by odq_parent_id) R 
-where
-D.odq_object_id = R.odq_parent_id ;
-
-/* Glisser le jeu d'essais de 20 jours */
-update odq_dwh.dm_processes AS m 
-set period_id = m.period_id+20,
-period_day = p.period_day
-from odq_dwh.dim_time AS p
-where p.period_id = m.period_id+20 ;
-
-/* Etendre le jeu d'essais de 10 jours */
-
-insert into odq_dwh.dm_processes(PLAYGROUND_ID,ODQ_OBJECT_ID,ODQ_PARENT_ID,ODQ_OBJECT_NAME, ODQ_OBJECT_CODE, ODQ_OBJECT_URL, PERIOD_DAY,ALL_RECORDS,ERROR_COUNT,SCORE,WORKLOAD,ADDED_VALUE,MAINTENANCE_COST,PERIOD_ID,CREATED_BY,UPDATED_BY,CREATED_AT,UPDATED_AT,TERRITORY_ID,ORGANISATION_ID) 
-select M.PLAYGROUND_ID,ODQ_OBJECT_ID,ODQ_PARENT_ID,ODQ_OBJECT_NAME, ODQ_OBJECT_CODE, ODQ_OBJECT_URL, T.PERIOD_DAY,ALL_RECORDS,ERROR_COUNT,SCORE,WORKLOAD,ADDED_VALUE,MAINTENANCE_COST, T.PERIOD_ID,M.CREATED_BY,M.UPDATED_BY,M.CREATED_AT,M.UPDATED_AT,0,0
-from odq_dwh.dm_processes M inner join odq_dwh.dim_time T on T.period_id = M.period_id + 10; 
-
 /* Generate unique object identifier for loading into Data Marts by concatenating id with object type:
 0: playground
 1: landscape
@@ -131,7 +111,7 @@ from odq_dwh.dm_processes M inner join odq_dwh.dim_time T on T.period_id = M.per
 */
 insert into odq_dwh.dm_processes(PLAYGROUND_ID,ODQ_OBJECT_ID,ODQ_PARENT_ID,ODQ_OBJECT_NAME, ODQ_OBJECT_CODE, ODQ_OBJECT_URL, PERIOD_DAY, ALL_RECORDS,ERROR_COUNT,SCORE,WORKLOAD,ADDED_VALUE,MAINTENANCE_COST,PERIOD_ID,CREATED_BY,UPDATED_BY,CREATED_AT,UPDATED_AT, TERRITORY_ID, ORGANISATION_ID) 
 select DWH.PLAYGROUND_ID, DWH.ID, DWH.PARENT_ID, DWH.NAME, DWH.CODE, DWH.URL, DTIME.PERIOD_DAY, DWH.ALL_RECORDS, DWH.ERROR_COUNT, DWH.SCORE, DWH.WORKLOAD, DWH.ADDED_VALUE, DWH.MAINTENANCE_COST, DTIME.PERIOD_ID,'Rake','Rake',current_timestamp, current_timestamp, 0, 0 from (
-select BR.playground_id||'0', BR.id||'7', BR.business_process_id||'6' parent_id, BR.name, BR.code, '/business_rules/'||BR.id url, to_char(current_date, 'YYYYMMDD') Period_day,  
+select BR.playground_id*10 playground_id, BR.id*10+7 id, BR.business_process_id*10+6 parent_id, BR.name, BR.code, '/business_rules/'||BR.id url, to_char(current_date, 'YYYYMMDD') Period_day,  
 br.all_records, 
 br.bad_records error_count,
 0 score,
@@ -139,9 +119,9 @@ br.bad_records error_count,
 0 added_value,
 0 maintenance_cost
 from odq_app.business_rules BR 
-group by BR.playground_id||'0', BR.id||'7', BR.business_process_id||'6', BR.name, BR.code, '/business_rules/'||BR.id,  to_char(current_date, 'YYYYMMDD') 
+/*group by BR.playground_id*10, BR.id*10+7, BR.business_process_id*10+6, BR.name, BR.code, '/business_rules/'||BR.id,  to_char(current_date, 'YYYYMMDD') */
 UNION
-select BP.playground_id||'0', BP.id||'6', BP.business_flow_id||'5' parent_id, BP.name, BP.code, '/business_processes/'||BP.id url, to_char(current_date, 'YYYYMMDD') Period_day, 
+select BP.playground_id*10 playground_id, BP.id*10+6 id, BP.business_flow_id*10+5 parent_id, BP.name, BP.code, '/business_processes/'||BP.id url, to_char(current_date, 'YYYYMMDD') Period_day, 
 bp.all_records, 
 bp.bad_records  error_count,
 0 score,
@@ -150,9 +130,9 @@ bp.bad_records  error_count,
 0 maintenance_cost
 from odq_app.business_processes BP 
 inner join odq_app.business_rules BR on BR.business_process_id = BP.id
-group by BP.playground_id||'0',  BP.id||'6', BP.business_flow_id||'5', BP.name, BP.code, '/business_processes/'||BP.id, to_char(current_date, 'YYYYMMDD')
+/*group by BP.playground_id*10,  BP.id*10+6, BP.business_flow_id*10+5', BP.name, BP.code, '/business_processes/'||BP.id, to_char(current_date, 'YYYYMMDD')*/
 UNION
-select BF.playground_id||'0', BF.id||'5', BF.business_area_id||'4' parent_id, BF.name, BF.code, '/business_flows/'||BF.id url, to_char(current_date, 'YYYYMMDD') Period_day,  
+select BF.playground_id*10 playground_id, BF.id*10+5 id, BF.business_area_id*10+4 parent_id, BF.name, BF.code, '/business_flows/'||BF.id url, to_char(current_date, 'YYYYMMDD') Period_day,  
 bf.all_records, 
 bf.bad_records  error_count,
 0 score,
@@ -162,9 +142,9 @@ bf.bad_records  error_count,
 from odq_app.business_flows BF 
 inner join odq_app.business_processes BP on BP.business_flow_id = BF.id
 inner join odq_app.business_rules BR on BR.business_process_id = BP.id
-group by BF.playground_id||'0', BF.id||'5', BF.business_area_id||'4', BF.name, BF.code, '/business_flows/'||BF.id, to_char(current_date, 'YYYYMMDD')
+/*group by BF.playground_id*10, BF.id*10+5, BF.business_area_id*10+4, BF.name, BF.code, '/business_flows/'||BF.id, to_char(current_date, 'YYYYMMDD')*/
 UNION
-select BA.playground_id||'0', BA.id||'4', BA.playground_id||'0' parent_id, BA.name, BA.code, '/business_areas/'||BA.id url, to_char(current_date, 'YYYYMMDD') Period_day,
+select BA.playground_id*10 playground_id, BA.id*10+4 id, BA.playground_id*10 parent_id, BA.name, BA.code, '/business_areas/'||BA.id url, to_char(current_date, 'YYYYMMDD') Period_day,
 ba.all_records, 
 ba.bad_records  error_count,
 0 score,
@@ -175,9 +155,9 @@ from odq_app.business_areas BA
 inner join odq_app.business_flows BF on BF.business_area_id = BA.id
 inner join odq_app.business_processes BP on BP.business_flow_id = BF.id
 inner join odq_app.business_rules BR on BR.business_process_id = BP.id
-group by BA.playground_id||'0',  BA.id||'4', BA.playground_id||'0', BA.name, BA.code, '/business_areas/'||BA.id, to_char(current_date, 'YYYYMMDD')
+/*group by BA.playground_id*10,  BA.id*10+4, BA.playground_id*10, BA.name, BA.code, '/business_areas/'||BA.id, to_char(current_date, 'YYYYMMDD')*/
 UNION
-select PG.id||'0', PG.id||'0', PG.id||'0' parent_id, PG.name, PG.code, '/playgrounds/'||PG.id url, to_char(current_date, 'YYYYMMDD') Period_day,
+select PG.id*10 playground_id, PG.id*10 id, PG.id*10 parent_id, PG.name, PG.code, '/playgrounds/'||PG.id url, to_char(current_date, 'YYYYMMDD') Period_day,
 pg.all_records, 
 pg.bad_records  error_count,
 0 score,
@@ -189,7 +169,7 @@ inner join odq_app.business_areas BA on BA.playground_id = PG.id
 inner join odq_app.business_flows BF on BF.business_area_id = BA.id
 inner join odq_app.business_processes BP on BP.business_flow_id = BF.id
 inner join odq_app.business_rules BR on BR.business_process_id = BP.id
-group by PG.id||'0', PG.id||'0', PG.id||'0', PG.name, PG.code, '/playgrounds/'||PG.id, to_char(current_date, 'YYYYMMDD')
+/*group by PG.id*10, PG.id*10, PG.id*10, PG.name, PG.code, '/playgrounds/'||PG.id, to_char(current_date, 'YYYYMMDD')*/
 )  DWH
 inner join odq_dwh.DIM_TIME DTIME on DTIME.PERIOD_DATE between current_date -20 and current_date
-where dwh.playground_id = 99;
+where dwh.playground_id = 990;
